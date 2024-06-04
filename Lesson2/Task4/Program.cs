233c65cf77faf633bdd3559020bb0f42ce2eeeeb
Stanislav krasko <staskrasko1991krasko@gmail.com>
2024-06-04 22:06:17 +0300

    Added Lesson2;Task4_2

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -1,4 +1,6 @@
-﻿int[] arr = { 77, 4, 5, 44, 2, 7, 9 };
+﻿using System.Globalization;
+
+int[] arr = { 77, 4, 5, 44, 2, 7, 9 };
 int i = 0;
 int max = arr[i];
 
@@ -10,6 +12,27 @@ while (i < arr.Length)
     }
     i++;
 }
-Console.Write(max);
+Console.WriteLine(max);
+
+int max1 = arr[0];
+for(int j = 0; j < arr.Length; j++)
+{
+    if(arr[j] > max)
+    {
+        max1 = arr[j];
+    }
+}
+Console.WriteLine(max1);
+
+max = arr[0];
+foreach(int e in arr)
+{
+    if(e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
+
 
 
