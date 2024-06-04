231d4e1831eb44392b00e44aa3457e62c5a2a04f
Stanislav krasko <staskrasko1991krasko@gmail.com>
2024-06-04 20:32:08 +0300

    Added Lesson2;Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr = { 1, 5, 4, 6, 9, 3, 2, 5, 4, 1 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    //i = i + 1;
+    i++;
+}
+
