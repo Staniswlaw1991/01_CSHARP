329e112b8e4d912a368e53eb55734beeaba9c92f
Stanislav krasko <staskrasko1991krasko@gmail.com>
2024-06-04 21:40:31 +0300

    Added Lesson2;Task4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = { 77, 4, 5, 44, 2, 7, 9 };
+int i = 0;
+int max = arr[i];
+
+while (i < arr.Length)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i++;
+}
+Console.Write(max);
+
+
