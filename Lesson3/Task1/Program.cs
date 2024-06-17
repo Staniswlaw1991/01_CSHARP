a6419ddc2803989b3efb7b2f67c6e126716ef7db
Stanislav krasko <staskrasko1991krasko@gmail.com>
2024-06-17 19:47:17 +0300

    added lesson3:task1

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
+
+double resul = CalculateFormula(2, 5, 7, 4);
+Console.WriteLine(resul);
